5d39f9782afbd652961b1976be59a3d684138292
Cristina Luengo <cristinaluengoagullo@gmail.com>
2015-10-29 18:02:49 +0100

    Monte Carlo change

--- MonteCarlo.cpp
+++ MonteCarlo.cpp
@@ -5,12 +5,30 @@ MonteCarlo::MonteCarlo() {}
 int MonteCarlo::selectMedian(vector<int> input)
 {
   vector<int> sample = sampleSet(input);
+  #ifdef DEBUG
+  cout << endl << endl << "Sample: " << endl;
+  for(auto s : sample) cout << s << " ";
+  cout << endl;
+  #endif
   sort(sample.begin(),sample.end());
+  #ifdef DEBUG
+  cout << endl << "Sorted sample: " << endl;
+  for(auto s : sample) cout << s << " ";
+  cout << endl << endl;
+  #endif
   int n = input.size();
   int index = floor(0.5*pow(n,0.75) - sqrt(n));
+  #ifdef DEBUG
+  cout << "index d = " << index << endl;
+  #endif
   if(index > 0) index--;
+  else index = 0;
   int d = sample[index];
   index = ceil(0.5*pow(n,0.75) + sqrt(n)) - 1;
+  if(index > sample.size()) index = sample.size()-1;
+  #ifdef DEBUG
+  cout << "index u = " << index << endl;
+  #endif
   int u = sample[index];
   set<int> c;
   int ld = 0, lu = 0;
@@ -19,12 +37,27 @@ int MonteCarlo::selectMedian(vector<int> input)
     if(input[i] < d) ld++;
     if(input[i] > u) lu++;
   }
-  if(ld > float(n/2) or lu > float(n/2)) return -1;
+  #ifdef DEBUG
+  cout << "d = " << d << ", u = " << u << endl;
+  cout << "ld = " << ld << ", lu = " << lu << endl;
+  cout << endl << "C set: " << endl;
+  for(auto s : c) cout << s << " ";
+  cout << endl;
+  #endif
+  if(ld > float(n/2) or lu > float(n/2)) {
+    #ifdef DEBUG
+    cout << "Entra fail1" << endl; 
+    #endif
+    return -1;
+  }
   if(c.size() <= 4*pow(n,0.75)) {
     set<int>::iterator it = c.begin();
     advance(it,floor(float(n/2))-ld);
     return *it;
   }
+  #ifdef DEBUG
+  cout << "Entra fail2" << endl;
+  #endif
   return -1;
 }
 
@@ -32,7 +65,7 @@ int MonteCarlo::selectK(vector<int> input, int k)
 {
   vector<int> sample = sampleSet(input);
   #ifdef DEBUG
-  cout << endl << "Sample: " << endl;
+  cout << endl << endl << "Sample: " << endl;
   for(auto s : sample) cout << s << " ";
   cout << endl;
   #endif
@@ -40,13 +73,21 @@ int MonteCarlo::selectK(vector<int> input, int k)
   #ifdef DEBUG
   cout << endl << "Sorted sample: " << endl;
   for(auto s : sample) cout << s << " ";
-  cout << endl;
+  cout << endl << endl;
   #endif
   int n = input.size();
-  int index = floor(0.5*pow(n,0.75) - sqrt(n));
+  int index = floor((k/n)*pow(n,0.75) - sqrt(n));
   if(index > 0) index--;
+  else index = 0;
+  #ifdef DEBUG
+  cout << "index d = " << index << endl;
+  #endif
   int d = sample[index];
-  index = ceil(0.5*pow(n,0.75) + sqrt(n)) - 1;
+  index = ceil(((n-k)/n)*pow(n,0.75) + sqrt(n)) - 1;
+  if(index > sample.size()) index = sample.size()-1;
+  #ifdef DEBUG
+  cout << "index u = " << index << endl;
+  #endif
   int u = sample[index];
   set<int> c;
   int ld = 0, lu = 0;
@@ -58,6 +99,9 @@ int MonteCarlo::selectK(vector<int> input, int k)
   #ifdef DEBUG
   cout << "d = " << d << ", u = " << u << endl;
   cout << "ld = " << ld << ", lu = " << lu << endl;
+  cout << endl << "C set: " << endl;
+  for(auto s : c) cout << s << " ";
+  cout << endl;
   #endif
   if(ld > k or lu > n-k) {
     #ifdef DEBUG
