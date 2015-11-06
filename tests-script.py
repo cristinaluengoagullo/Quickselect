f487451fdbf0b1485e91d03f195ec5f69e1a33d7
Cristina Luengo <cristinaluengoagullo@gmail.com>
2015-11-06 11:43:36 +0100

    Tests

---
+++ tests-script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import subprocess
+import math
+
+lengths = [10001,50001,100001,500001,1000001,5000001,10000001,50000001,100000001]
+for i in range(0,len(lengths)):
+  print ""
+  k = lengths[i]/2
+  args = ['./main',str(lengths[i])]
+  subprocess.call(args)
+  print""
